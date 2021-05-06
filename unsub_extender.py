5988eb5cdee3f259752a7c061df30bec384bd4bf
eschares <38474105+eschares@users.noreply.github.com>
2021-05-06 10:13:40 -0500

    Both back in

--- unsub_extender.py
+++ unsub_extender.py
@@ -8,15 +8,25 @@ import streamlit as st
 import streamlit.components.v1 as components
 st.markdown(
     """
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-196264375-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FDDMR7WRFB"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-FDDMR7WRFB');
+    </script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-196264375-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-    gtag('config', 'UA-196264375-1');
-</script>
+      gtag('config', 'UA-196264375-1');
+    </script>
 """,
     unsafe_allow_html=True
 )
