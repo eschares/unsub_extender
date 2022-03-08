243fca7b78cc646a00d7c547cec54adbd5e3e9b7
Eric Schares <38474105+eschares@users.noreply.github.com>
2022-03-08 10:58:56 -0600

    Simplifying webinar link

--- unsub_extender.py
+++ unsub_extender.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title='Unsub Extender', page_icon="scissors.jpg", layout
 
 #st.set_page_config(layout="wide")
 st.image('unsub_extender2.png')
-st.markdown("### ** :wave: Learn more about Unsub Extender from a recent webinar and live demo: [Feb 22](https://vimeo.com/680578098) or [Feb 24](https://vimeo.com/681619823) **")
+st.markdown("### ** :wave: Learn more about Unsub Extender by watching a recent [live demo](https://vimeo.com/680578098) **")
 
 
 with st.expander("How to use:"):
